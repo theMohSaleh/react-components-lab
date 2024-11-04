739055f9dbda07291f435f09333cde025579ae52
Mohamed Saleh <mohxbox36021@gmail.com>
2024-11-04 11:17:49 +0300

    finished the lab

---
+++ src/components/WeatherForecast/WeatherForecast.jsx
@@ -0,0 +1,13 @@
+import './WeatherForecast.css'
+function WeatherForecast({forecast}) {
+    return (
+        <div className='weather'>
+            <h2>{forecast.day}</h2>
+            <img src={forecast.img} alt={forecast.imgAlt} />
+            <p><span>conditions: </span>{forecast.conditions}</p>
+            <p><span>time: </span>{forecast.time}</p>
+        </div>
+    )
+}
+
+export default WeatherForecast
